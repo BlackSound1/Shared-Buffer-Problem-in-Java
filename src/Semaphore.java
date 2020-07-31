059e152534857b32b3430dd2a308ecb90559ecbb
BlackSound1 <matthewr@videotron.ca>
2020-07-31 12:20:45 -0400

    Just added some files and partially defined Semaphore class

---
+++ src/Semaphore.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Semaphore {
+    private int value;
+    private ArrayList<Thread> list;
+
+    public Semaphore() {
+        value = 0;
+        list = null;
+    }
+
+    public Semaphore(int value) {
+        this.value = value;
+        list = null;
+    }
+
+    public int getValue() { return value; }
+
+    public void setValue(int value) { this.value = value; }
+
+    public ArrayList<Thread> getList() { return list; }
+
+    public void setList(ArrayList<Thread> list) { this.list = list; }
+
+    public static void wait(Semaphore s){
+        s.value--;
+        /*if (s.value < 0){
+
+        }*/
+    }
+
+    public static void signal(Semaphore s){
+        s.value++;
+        /*if (s.value <= 0){
+
+        }*/
+    }
+}
